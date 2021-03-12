00ed7a69e461497412c489daabc5278dafb8d451
Andy <72276976+Andy-math@users.noreply.github.com>
2021-03-12 18:49:33 +0800

    update

--- likelihood/likelihood.py
+++ likelihood/likelihood.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, List, Optional, Tuple
+from typing import Any, List
 
 import numpy
 from numerical.typedefs import ndarray
@@ -67,44 +67,6 @@ _Convolution_gradinfo_t = type(None)
 class Convolution(Stage[_Convolution_gradinfo_t]):
     pass
 """
-
-
-_LogNormpdf_gradinfo_t = ndarray
-
-
-class LogNormpdf(Logpdf[_LogNormpdf_gradinfo_t]):
-    def __init__(self, variance_name: str, input: Tuple[int, int], output: int) -> None:
-        super().__init__([variance_name], input, (output,))
-
-    def _eval(
-        self, var: ndarray, mu_x: ndarray, *, grad: bool
-    ) -> Tuple[ndarray, Optional[_LogNormpdf_gradinfo_t]]:
-        """
-        x ~ N(0, Var)
-        p(x) = 1/sqrt(Var*2pi) * exp{ -(x*x)/(2Var) }
-        log p(x) = -1/2{ log(Var) + log(2pi) } - (x*x)/(2Var)
-                    = (-1/2) { log(Var) + log(2pi) + (x*x)/Var }
-        """
-        x: ndarray = mu_x[:, [1]] - mu_x[:, [0]]  # type: ignore
-        constant = numpy.log(var) + numpy.log(2.0) + numpy.log(numpy.pi)
-        logP = (-1.0 / 2.0) * (constant + (x * x) / var)
-        if not grad:
-            return logP, None
-        return logP, x
-
-    def _grad(
-        self, var: ndarray, x: _LogNormpdf_gradinfo_t, dL_dlogP: ndarray
-    ) -> Tuple[ndarray, ndarray]:
-        """
-        d/dx{log p(x)} = (-1/2) { 2x/Var } = -x/Var
-        d/dVar{log p(x)} = (-1/2) {1/Var - (x*x)/(Var*Var)}
-                            = (1/2) {(x/Var) * (x/Var) - 1/Var}
-        """
-        z = x / var
-        dL_di = dL_dlogP * -z
-        dL_dlogP.shape = (dL_dlogP.shape[0],)
-        dL_dc = dL_dlogP @ ((1.0 / 2.0) * (z * z - 1.0 / var))
-        return dL_di, dL_dc
 
 
 class negLikelihood:
--- tests/test_linear_regress.py
+++ tests/test_linear_regress.py
@@ -3,6 +3,7 @@ import numpy
 import numpy.linalg
 from likelihood import likelihood
 from likelihood.stages.Linear import Linear
+from likelihood.stages.LogNormpdf import LogNormpdf
 from numerical import difference
 from numerical.typedefs import ndarray
 from optimizer import trust_region
@@ -17,7 +18,7 @@ def run_once(n: int, m: int, seed: int = 0) -> None:
     abserr_decomp = difference.absolute(beta, beta_decomp)
 
     stage1 = Linear(["b1", "b2", "b3", "b4", "b5", "b0"], list(range(1, 7)), [1])
-    stage2 = likelihood.LogNormpdf("var", (0, 1), 0)
+    stage2 = LogNormpdf("var", (0, 1), 0)
     nll = likelihood.negLikelihood([stage1, stage2], 7)
 
     beta0 = numpy.zeros((beta.shape[0] + 1,))
