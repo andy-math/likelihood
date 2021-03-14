d7d985bab16057c168e4f949b9be1e069656c675
Andy <72276976+Andy-math@users.noreply.github.com>
2021-03-14 15:09:25 +0800

    Update Midas_exp.py

--- likelihood/stages/Midas_exp.py
+++ likelihood/stages/Midas_exp.py
@@ -1,41 +1,51 @@
 from typing import Sequence, Tuple
 
 import numpy
+from likelihood.KnownIssue import KnownIssue
 from likelihood.stages.abc.Convolution import Convolution
 from numerical.typedefs import ndarray
 
 
 class Midas_exp(Convolution):
-    k: int
+    K: int
 
     def __init__(
         self, names: str, input: Sequence[int], output: Sequence[int], *, k: int
     ) -> None:
         assert len(input) == len(output)
         super().__init__((names,), input, output)
-        self.k = k
+        self.K = k
 
-    def kernel(self, omega: ndarray) -> Tuple[ndarray, ndarray]:
+    def kernel(self, _omega: ndarray) -> Tuple[ndarray, ndarray]:
         """
-        rphi(1<= k <= K) = omega ** k
+        rphi(1 <= k <= K) = omega ** k
         phi = rphi/sum(rphi)
-        0 < omega < 1
         """
-        k = numpy.arange(1.0, self.k + 1.0)
+        k = numpy.arange(1.0, self.K + 1.0)
+        k.shape = (k.shape[0], 1)
+        omega = float(_omega)
 
+        """
+        rphi = omega ** k
+        drphi_do = k * omega ** (k-1)
+        """
         rphi = omega ** k
         drphi_do = k * omega ** (k - 1.0)
 
-        if max(rphi) == 0:
-            assert False
-
         sum = numpy.sum(rphi)
-        dsum_do = numpy.sum(drphi_do)
+        dsum_do = numpy.sum(drphi_do, axis=0, keepdims=True)
+        if sum == 0:
+            raise KnownIssue("Midas_exp: 权重全为0")
 
+        """
+        phi = rphi/sum
+        dphi_do = (1/sum) * drphi_do - rphi / (sum*sum) * dsum_do
+                = drphi_do / sum - dsum_do * phi / sum
+        """
         phi = rphi / sum
-        dphi_do = drphi_do / sum - rphi * dsum_do / (sum * sum)
+        dphi_do = drphi_do / sum - dsum_do * phi / sum
 
-        dphi_do.shape = (dphi_do.shape[0], 1)
+        phi.shape = (phi.shape[0],)
         return phi, dphi_do
 
     def get_constraint(self) -> Tuple[ndarray, ndarray, ndarray, ndarray]:
