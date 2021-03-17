f7d554752753361dd530d1083cac8b23313d36ff
Andy <72276976+Andy-math@users.noreply.github.com>
2021-03-17 16:14:26 +0800

    lasso测试用例

--- tests/test_lasso_linear.py
+++ tests/test_lasso_linear.py
@@ -13,22 +13,20 @@ from optimizer import trust_region
 def run_once(n: int, m: int, seed: int = 0) -> None:
     numpy.random.seed(seed)
     rrrr = numpy.random.randn(n, m)
-    x = numpy.concatenate((rrrr, rrrr, numpy.ones((n, 1))), axis=1)
-    beta = numpy.random.randn(2 * m + 1)
+    rrrr[:, -1] = rrrr[:, 0] - rrrr[:, -1] / 1000
+    x = rrrr
+    beta = n * numpy.random.randn(m)
+    beta[-1] = 0.0
     y = x @ beta + numpy.random.randn(n)
     beta_decomp, _, _, _ = numpy.linalg.lstsq(x, y, rcond=None)  # type: ignore
-    abserr_decomp = difference.absolute(
-        beta[:m] + beta[m:-1], beta_decomp[:m] + beta_decomp[m:-1]
-    )
+    relerr_decomp = difference.relative(beta[:-1], beta_decomp[:-1])
 
-    stage1 = Linear(
-        [*[f"b{i}" for i in range(1, 2 * m + 1)], "b0"], list(range(1, 2 * m + 2)), [1]
-    )
+    stage1 = Linear([f"b{i}" for i in range(1, m + 1)], list(range(1, m + 1)), [1])
     stage2 = LogNormpdf("var", (0, 1), (0, 1))
     penalty = Lasso(1.0, (0, 1), 0)
-    nll = likelihood.negLikelihood([stage1, stage2], penalty, nvars=2 * m + 2)
+    nll = likelihood.negLikelihood([stage1, stage2], penalty, nvars=m + 1)
 
-    beta0 = numpy.zeros((beta.shape[0] + 1,))
+    beta0 = numpy.zeros((beta.shape[0] + 1))
     beta0[-1] = 1.0
     input = numpy.concatenate((y.reshape((-1, 1)), x), axis=1)
 
@@ -43,6 +41,7 @@ def run_once(n: int, m: int, seed: int = 0) -> None:
         return nll.grad(x, input, regularize=True)
 
     opts = trust_region.Trust_Region_Options(max_iter=300)
+    opts.check_iter = 20
 
     constraint = nll.get_constraint()
 
@@ -54,25 +53,22 @@ def run_once(n: int, m: int, seed: int = 0) -> None:
         opts,
     )
     beta_mle = result.x[:-1]
-    abserr_mle = difference.absolute(
-        beta[:m] + beta[m:-1], beta_mle[:m] + beta_mle[m:-1]
-    )
+    relerr_mle = difference.relative(beta[:-1], beta_mle[:-1])
     print("result.success: ", result.success)
-    print("beta:   ", beta[:-1])
-    print("decomp: ", beta_decomp[:-1])
-    print("mle:    ", beta_mle[:-1])
-    print("abserr_decomp: ", abserr_decomp)
-    print("abserr_mle:    ", abserr_mle)
-    return
+    print("beta:   ", beta)
+    print("decomp: ", beta_decomp)
+    print("mle:    ", beta_mle)
+    print("relerr_decomp: ", relerr_decomp)
+    print("relerr_mle:    ", relerr_mle)
     assert result.success
-    assert 5 < result.iter < 20
-    assert abserr_decomp < 0.1
-    assert abserr_mle < 2 * abserr_decomp
+    assert 5 < result.iter < 100
+    assert relerr_decomp < 0.1
+    assert relerr_mle < relerr_decomp
 
 
 class Test_1:
     def test_1(self) -> None:
-        run_once(1000, 3)
+        run_once(1000, 4)
 
 
 if __name__ == "__main__":
