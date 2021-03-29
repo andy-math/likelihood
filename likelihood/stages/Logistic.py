97a3bb95f8e7431f92d2e4f790475d85ab04c5c9
Andy <72276976+Andy-math@users.noreply.github.com>
2021-03-29 15:20:08 +0800

    logistic实现

---
+++ likelihood/stages/Logistic.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import Optional, Tuple
+
+import numpy
+from likelihood.stages.abc.Stage import Stage
+from numerical.typedefs import ndarray
+
+_Logistic_gradinfo_t = ndarray
+
+
+class Logistic(Stage[_Logistic_gradinfo_t]):
+    def __init__(self, input: int, output: int) -> None:
+        super().__init__([], (input,), (output,))
+
+    def _eval(
+        self, _: ndarray, x: ndarray, *, grad: bool, debug: bool
+    ) -> Tuple[ndarray, Optional[_Logistic_gradinfo_t]]:
+        """
+        logistic(x) = exp(x)/(1+exp(x))
+                    = 1/(exp(-x)+1)  # exp overflow safety
+        """
+        output: ndarray = 1.0 / (numpy.exp(-x) + 1.0)
+        if not grad:
+            return output, None
+        return output, output
+
+    def _grad(
+        self, var: ndarray, output: _Logistic_gradinfo_t, dL_dR: ndarray, *, debug: bool
+    ) -> Tuple[ndarray, ndarray]:
+        """
+        dL_dx = -1*1/(exp(-x)+1)^2 * exp(-x) * -1
+              = 1/(exp(-x)+1) * exp(-x)/(exp(-x)+1)
+              = logistic * (1-logistic)
+        """
+        return dL_dR * (output * (1.0 - output)), numpy.empty((0,))
+
+    def get_constraint(self) -> Tuple[ndarray, ndarray, ndarray, ndarray]:
+        A = numpy.empty((0, 0))
+        b = numpy.empty((0,))
+        lb = numpy.empty((0,))
+        ub = numpy.empty((0,))
+        return A, b, lb, ub
