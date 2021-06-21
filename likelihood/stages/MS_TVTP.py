939549df717460fcd70a0e6140a9ef9ec3c7579c
Andy <72276976+Andy-math@users.noreply.github.com>
2021-06-21 16:47:12 +0800

    bugfix

--- likelihood/stages/MS_TVTP.py
+++ likelihood/stages/MS_TVTP.py
@@ -377,7 +377,9 @@ class MS_TVTP(Iterative.Iterative, Logpdf.Logpdf[Iterative._Iterative_gradinfo_t
         data_out_names: Tuple[str, str, str],
     ) -> None:
         assert isinstance(submodels[0], type(submodels[1]))
-        assert len(submodels[0].data_in_names) == len(submodels[1].data_out_names)
+        assert isinstance(submodels[1], type(submodels[0]))
+        assert len(submodels[0].data_in_names) == len(submodels[1].data_in_names)
+        assert len(submodels[0].data_out_names) == len(submodels[1].data_out_names)
 
         super().__init__(
             (),
