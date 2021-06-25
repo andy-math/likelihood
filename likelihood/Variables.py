38c0fe048b580d901b9be28e29453f87a9762b58
Andy <72276976+Andy-math@users.noreply.github.com>
2021-06-25 15:40:43 +0800

    为Variables添加date列强制支持

--- likelihood/Variables.py
+++ likelihood/Variables.py
@@ -1,19 +1,29 @@
 from __future__ import annotations
 
-from typing import Optional, Tuple
+from datetime import datetime
+from typing import Generic, Optional, Tuple, TypeVar
 
 import numpy
 import overloads.dyn_typing as dynT
 from numerical.typedefs import ndarray
 from overloads.shortcuts import assertNoInfNaN, isunique
 
+T = TypeVar("T", int, datetime)
 
-class Variables:
+
+class Variables(Generic[T]):
     data_names: Tuple[str, ...]
+    date: Tuple[T, ...]
     sheet: ndarray
 
-    def __init__(self, *datas: Tuple[str, Optional[ndarray]]) -> None:
+    def __init__(
+        self, date: Tuple[T, ...], *datas: Tuple[str, Optional[ndarray]]
+    ) -> None:
         length_tracker = dynT.SizeVar()
+        assert dynT.Union(
+            dynT.List(dynT.Class(datetime), length_tracker),
+            dynT.List(dynT.Int(), length_tracker),
+        )._isinstance(list(date))
         for d in datas:
             assert dynT.Tuple(
                 (
@@ -29,19 +39,28 @@ class Variables:
 
         zeros = numpy.zeros((length_tracker.value,))
         self.data_names = data_names
+        self.date = date
         self.sheet = numpy.stack(
             [var if var is not None else zeros for _, var in datas], axis=1
         )
 
-    def index(self, *, from_: int, to: int) -> Variables:
+    def index(self, *, from_: int, to: int) -> Variables[T]:
         return Variables(
-            *((name, self.sheet[from_:to, i]) for i, name in enumerate(self.data_names))
+            self.date[from_:to],
+            *(
+                (name, self.sheet[from_:to, i])
+                for i, name in enumerate(self.data_names)
+            ),
         )
 
-    def subset(self, *data_names: str) -> Variables:
+    def subset(self, *data_names: str) -> Variables[T]:
         assert isunique(data_names)
         for name in data_names:
             assert name in self.data_names, f"变量{name}未出现于此变量表中"
         return Variables(
-            *((name, self.sheet[:, self.data_names.index(name)]) for name in data_names)
+            self.date,
+            *(
+                (name, self.sheet[:, self.data_names.index(name)])
+                for name in data_names
+            ),
         )
