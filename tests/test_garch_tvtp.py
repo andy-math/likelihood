83b9087add49e1723e419b80da3b0b29cff3ae2e
Andy <72276976+Andy-math@users.noreply.github.com>
2021-07-23 06:16:21 +0800

    Update test_garch_tvtp.py

--- tests/test_garch_tvtp.py
+++ tests/test_garch_tvtp.py
@@ -103,7 +103,7 @@ def run_once(coeff: ndarray, n: int, seed: int = 0) -> None:
     constraint = nll.get_constraints()
 
     opts = trust_region.Trust_Region_Options(max_iter=99999)
-    opts.check_iter = 15
+    opts.check_iter = 5
     opts.check_rel = 5e-2
     opts.tol_grad = 1e-4
     opts.border_abstol = 1e-10
